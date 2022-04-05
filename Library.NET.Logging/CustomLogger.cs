f8b68795e430ce56781efc99ba14cec16e3234c7
CrimsonOrion <crimsonorion@gmail.com>
2022-04-05 11:53:13 -0400

    v2202.4.5.1 - Streamlined CustomLogger to allow output to only console - Added documentation

--- Library.NET.Logging/CustomLogger.cs
+++ Library.NET.Logging/CustomLogger.cs
@@ -1,18 +1,195 @@
-﻿namespace Library.NET.Logging;
+﻿using System.Diagnostics;
+
+namespace Library.NET.Logging;
+
+/// <summary>
+/// Custom logger that sends messages of various levels to a log file and/or console
+/// </summary>
 public class CustomLogger : ICustomLogger
 {
-    public FileInfo LogFileInfo { get; set; }
-    public bool OutputToConsole { get; set; }
-    public LogLevel LogLevel { get; set; }
+    public FileInfo LogFileInfo { get; set; } = null;
+    public bool OutputToConsole { get; set; } = true;
+    public LogLevel LogLevel { get; set; } = LogLevel.Debug;
 
+    /// <summary>
+    /// Custom logger that sends messages of various levels to a log file and, if desired, the console
+    /// </summary>
+    /// <param name="logFileInfo"><see cref="FileInfo"/> for the log file</param>
+    /// <param name="outputToConsole">Sets if you want log data to output to the console</param>
+    /// <param name="logLevel">Set the <see cref="Logging.LogLevel"/> to filter the information that gets passed to the logger</param>
     public CustomLogger(FileInfo logFileInfo, bool outputToConsole, LogLevel logLevel)
     {
         LogFileInfo = logFileInfo;
         OutputToConsole = outputToConsole;
         LogLevel = logLevel;
     }
+
+    /// <summary>
+    /// Custom logger that sends messages to the console
+    /// </summary>
+    /// <param name="logLevel">Set the <see cref="Logging.LogLevel"/> to filter the information that gets passed to the logger</param>
+    public CustomLogger(LogLevel logLevel) => LogLevel = logLevel;
+
+    /// <summary>
+    /// Custom logger that sends messages to the console with <see cref="Logging.LogLevel.Debug"/> and higher
+    /// </summary>
+    public CustomLogger() { }
+
+    public void LogDebug(string message)
+    {
+        if (LogLevel > LogLevel.Debug)
+        {
+            return;
+        }
+
+        var line = $"[Debug] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}";
+
+        Debug.WriteLine(line);
+
+        if (OutputToConsole)
+        {
+            Console.WriteLine(line); // Don't change the color.
+        }
+    }
+
+    public void LogInformation(string message)
+    {
+        if (LogLevel > LogLevel.Information)
+        {
+            return;
+        }
+
+        var line = $"[Information] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}";
+
+        if (LogFileInfo != null)
+        {
+            File.AppendAllText(LogFileInfo.FullName, line + $"\r\n");
+        }
+
+        if (OutputToConsole)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(line);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+
+    public void LogWarning(Exception exception, string? message)
+    {
+        if (LogLevel > LogLevel.Warning)
+        {
+            return;
+        }
+
+        var exceptionMessage = $"Exception Message:{exception.Message}";
+        var line = string.IsNullOrEmpty(message)
+            ? $"[Warning] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {exceptionMessage}"
+            : $"[Warning] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}\r\n\t{exceptionMessage}";
+
+        if (LogFileInfo != null)
+        {
+            File.AppendAllText(LogFileInfo.FullName, line + $"\r\n");
+        }
+
+        if (OutputToConsole)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(line);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+
+    public void LogTrace(Exception exception, string? message)
+    {
+        if (LogLevel > LogLevel.Trace)
+        {
+            return;
+        }
+
+        var exceptionMessage = $"Exception Message:{exception.Message}";
+        var innerException = $"Inner Message:{exception.InnerException}";
+        var stackTrace = $"Stack Trace:{exception.StackTrace}";
+        var line = string.IsNullOrEmpty(message)
+            ? $"[Error] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {exceptionMessage}\r\n\t{innerException}\r\n\t{stackTrace}"
+            : $"[Error] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}\r\n\t{exceptionMessage}\r\n\t{innerException}\r\n\t{stackTrace}";
+
+        if (LogFileInfo != null)
+        {
+            File.AppendAllText(LogFileInfo.FullName, line + $"\r\n");
+        }
+
+        if (OutputToConsole)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(stackTrace);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+
+    public void LogError(Exception exception, string? message)
+    {
+        if (LogLevel > LogLevel.Error)
+        {
+            return;
+        }
+
+        var exceptionMessage = $"Exception Message:{exception.Message}";
+        var innerException = $"Inner Message:{exception.InnerException}";
+        var stackTrace = $"Stack Trace:{exception.StackTrace}";
+        var line = string.IsNullOrEmpty(message)
+            ? $"[Error] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {exceptionMessage}\r\n\t{innerException}"
+            : $"[Error] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}\r\n\t{exceptionMessage}\r\n\t{innerException}";
+
+        if (LogFileInfo != null)
+        {
+            File.AppendAllText(LogFileInfo.FullName, line + $"\r\n");
+        }
+
+        if (OutputToConsole)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(stackTrace);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+
+    public void LogCritical(Exception exception, string? message)
+    {
+        var exceptionMessage = $"Exception Message:{exception.Message}";
+        var innerException = $"Inner Message:{exception.InnerException}";
+        var stackTrace = $"Stack Trace:{exception.StackTrace}";
+        var line = string.IsNullOrEmpty(message)
+            ? $"[Critical] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {exceptionMessage}\r\n\t{innerException}"
+            : $"[Critical] <{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}> : {message}\r\n\t{exceptionMessage}\r\n\t{innerException}";
+
+        if (LogFileInfo != null)
+        {
+            File.AppendAllText(LogFileInfo.FullName, line + $"\r\n");
+        }
+
+        if (OutputToConsole)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(stackTrace);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
 }
 
+/// <summary>
+/// Determines level of logging you want to track in your log
+/// </summary>
 public enum LogLevel
 {
     Debug,
--- Library.NET.Logging/Interfaces/ICustomLogger.cs
+++ Library.NET.Logging/Interfaces/ICustomLogger.cs
@@ -1,9 +1,61 @@
-﻿
-namespace Library.NET.Logging;
+﻿namespace Library.NET.Logging;
 
+/// <summary>
+/// Custom Logger that displays and/or writes messages to a log and/or console
+/// </summary>
 public interface ICustomLogger
 {
+    /// <summary>
+    /// <see cref="FileInfo"/> for the log file. Can be <c>null</c> to display only on the console.
+    /// </summary>
     FileInfo LogFileInfo { get; set; }
+    /// <summary>
+    /// Set the log level to filter the information that gets passed to the logger.
+    /// </summary>
+    /// <value>Debug, Informational, Warning, Trace, Error, Critical</value>
     LogLevel LogLevel { get; set; }
+    /// <summary>
+    /// Sets if you want log data to output to the console.
+    /// </summary>
     bool OutputToConsole { get; set; }
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Debug"/> with the attached <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">Message you want to appear with this entry</param>
+    void LogDebug(string message);
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Information"/> with the attached <paramref name="message"/>.
+    /// </summary>
+    /// <param name="message">Message you want to appear with this entry</param>
+    void LogInformation(string message);
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Warning"/> with the attached <paramref name="message"/> and <see cref="Exception.Message"/>. 
+    /// </summary>
+    /// <param name="exception"></param>
+    /// <param name="message"></param>
+    void LogWarning(Exception exception, string? message);
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Trace"/> with the attached <paramref name="message"/>, <see cref="Exception.Message"/> and <see cref="Exception.InnerException"/>. <see cref="Exception.StackTrace"/> is displayed in the console if the <see cref="OutputToConsole"/> is set to <c>true</c>.
+    /// </summary>
+    /// <param name="exception">The caught <see cref="Exception"/></param>
+    /// <param name="message">Optional message you want to appear with this entry</param>
+    void LogTrace(Exception exception, string? message);
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Error"/> with the attached <paramref name="message"/>, <see cref="Exception.Message"/> and <see cref="Exception.InnerException"/>. <see cref="Exception.StackTrace"/> is displayed in the console if the <see cref="OutputToConsole"/> is set to <c>true</c>.
+    /// </summary>
+    /// <param name="exception">The caught <see cref="Exception"/></param>
+    /// <param name="message">Optional message you want to appear with this entry</param>
+    void LogError(Exception exception, string? message);
+
+    /// <summary>
+    /// Creates a log entry with <see cref="Logging.LogLevel.Critical"/> with the attached <paramref name="message"/>, <see cref="Exception.Message"/> and <see cref="Exception.InnerException"/>. <see cref="Exception.StackTrace"/> is displayed in the console if the <see cref="OutputToConsole"/> is set to <c>true</c>.
+    /// </summary>
+    /// <param name="exception">The caught <see cref="Exception"/></param>
+    /// <param name="message">Optional message you want to appear with this entry</param>
+    void LogCritical(Exception exception, string? message);
 }
