2545ac2b4fa2549ef29d201bb4c77771d95a62d3
CrimsonOrion <crimsonorion@gmail.com>
2022-04-07 11:54:40 -0400

    One more time... with feeling!

---
+++ Tester/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Library.NET.Cryptography;
+global using Library.NET.DataAccess;
+global using Library.NET.DataAccess.ConnectionStrings;
+global using Library.NET.Events;
+global using Library.NET.Helpers;
+global using Library.NET.Logging;
+global using Library.NET.Mailer;
+global using Library.NET.SFTP;
